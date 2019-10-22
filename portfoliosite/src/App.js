2612bb8d846ec85dd7c1a28dee966ed188c1c002
Kevin Benoit <kevin.benoit8@gmail.com>
2019-10-22 08:15:10 -0600

    navigation

---
+++ portfoliosite/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Navigation.js'
+function App() {
+  return (
+    <div className="App">
+       <Navigation></Navigation>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfoliosite/src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navigation= () => {
+    return (
+        <div className="">
+            <a href="" className='f1 link dim black pa pointer'>Kevin Benoit</a>
+            <a href="" className='f3 link dim blue  pa2 pointer'>Resume</a>
+            <a href="" className="f3 link dim blue  pa2 pointer">Portfolio</a>
+            <a href="" className="f3 link dim blue  pa2 pointer">Contact</a>
+            <hr ></hr>
+        </div>
+    );
+}
+
+export default Navigation;
