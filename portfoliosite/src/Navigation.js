4026aecc7242f6411e6c4a1ff49c46b835c8812a
Kevin Benoit <kevin.benoit8@gmail.com>
2019-10-22 09:58:21 -0600

    Open Sans fonts

--- portfoliosite/src/Navigation.js
+++ portfoliosite/src/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Navigation.css'
 
 const Navigation= () => {
     return (
         <div className="flex center pt3 pb2 bb">
-            <a href="" className='pr7 f1 link dim black pa2 pointer'>Kevin Benoit</a>
+            <a href="" className=' pr7 f1 link dim black pa2 pointer name'>Kevin Benoit</a>
             <a href="" className='pl7 f4 link dim blue  pa2 pointer'>Resume</a>
             <a href="" className="f4 link dim blue  pa2 pointer">Portfolio</a>
             <a href="" className="f4 link dim blue  pa2 pointer">Contact</a>
