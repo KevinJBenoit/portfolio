23607ad14b91790af388454567a607e6cec664ec
Kevin Benoit <kevin.benoit8@gmail.com>
2019-10-22 08:15:27 -0600

    nav flex

--- portfoliosite/src/App.js
+++ portfoliosite/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './Navigation.js'
 function App() {
   return (
-    <div className="App">
+    <div className="flex">
        <Navigation></Navigation>
     </div>
   );
--- portfoliosite/src/Navigation.js
+++ portfoliosite/src/Navigation.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navigation= () => {
     return (
-        <div className="">
-            <a href="" className='f1 link dim black pa pointer'>Kevin Benoit</a>
-            <a href="" className='f3 link dim blue  pa2 pointer'>Resume</a>
-            <a href="" className="f3 link dim blue  pa2 pointer">Portfolio</a>
-            <a href="" className="f3 link dim blue  pa2 pointer">Contact</a>
-            <hr ></hr>
+        <div className="flex center pt3 pb2 bb">
+            <a href="" className='pr7 f1 link dim black pa2 pointer'>Kevin Benoit</a>
+            <a href="" className='pl7 f4 link dim blue  pa2 pointer'>Resume</a>
+            <a href="" className="f4 link dim blue  pa2 pointer">Portfolio</a>
+            <a href="" className="f4 link dim blue  pa2 pointer">Contact</a>
+
         </div>
     );
 }
