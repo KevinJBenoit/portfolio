7163fb4d6c7e51f5dbeb0f463807f5d630450065
Kevin Benoit <kevin.benoit8@gmail.com>
2019-10-28 10:52:24 -0600

    Updates

---
+++ precache-manifest.f6ad11d61f7f9df2b75e4507729033e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398f02a273b4a6eae9b708259b4ce79f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3d52ea5fea57b106dee",
+    "url": "/portfolio/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "88a8fe4be842634f8d80",
+    "url": "/portfolio/static/css/main.6e89e75b.chunk.css"
+  },
+  {
+    "revision": "f3d52ea5fea57b106dee",
+    "url": "/portfolio/static/js/2.96353802.chunk.js"
+  },
+  {
+    "revision": "88a8fe4be842634f8d80",
+    "url": "/portfolio/static/js/main.b85c4e6c.chunk.js"
+  },
+  {
+    "revision": "9227878a495a1490e379",
+    "url": "/portfolio/static/js/runtime-main.4419d321.js"
+  }
+]);
